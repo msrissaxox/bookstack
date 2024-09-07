43bc4a843370d60f6abd40780d2f61762f8bbf70
msrissaxox <marissa.bianca.lamothe@gmail.com>
2024-09-07 00:32:03 -0400

    first commit

---
+++ mytailwind-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+// import Hero from './Hero.jsx';
+// import Feature from './Feature.jsx';
+// import Carousel from './Carousel.jsx';
+// import Stats from './Stats.jsx';
+import './index.css';
+
+import Login from './Login.jsx';
+import Footer from './Footer.jsx';
+
+export default function App() {
+  return (
+    <div>
+  <Login />
+  <Footer />
+    </div>
+  );
+}
---
+++ mytailwind-project/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+<App />
+<div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>  
+
+
+  </StrictMode>,
+)
+
+
